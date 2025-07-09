8f69bd7d6f39ce66fa2426f393bbc3821d18ec77
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2025-07-10 00:17:26 +0530

    Add graceful shutdown to HTTP server
    Implemented graceful shutdown for the HTTP server using context and signal handling. Added structured logging with slog and improved server startup and shutdown messages.

--- cmd/golang-students-api/main.go
+++ cmd/golang-students-api/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/AnshSinghSonkhia/golang-students-api/internal/config"
 )
 
 func main() {
-	// TODO: load config
+	// load config
 
 	cfg := config.MustLoad()
 
 	// TODO: database setup
 
-	// TODO: setup router
+	// setup router
 
 	router := http.NewServeMux()
 
@@ -23,18 +29,42 @@ func main() {
 		w.Write([]byte("Welcome to the Golang Students API!"))
 	})
 
-	// TODO: setup server
+	// setup server
 
 	server := http.Server{
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
 
+	slog.Info("Server started %s", slog.String("address", cfg.Addr)) // log the server address
+
 	fmt.Printf("Server is started and runnin %s\n", cfg.HTTPServer.Addr)
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatalf("Failed to start server: %s", err.Error())
-	}
+	// gracefully shutdown server on interrupt signal
+	done := make(chan os.Signal, 1)
 
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM) // catch interrupt signals
+
+	go func() { // run server in a goroutine
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("Failed to start server: %s", err.Error())
+		}
+	}()
+
+	// Wait for interrupt signal
+	<-done // block until an interrupt signal is received
+
+	slog.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second) // create a context with a timeout for graceful shutdown
+
+	defer cancel() // ensure the context is cancelled after use
+
+	err := server.Shutdown(ctx) // shutdown the server gracefully
+	if err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	} // log any error that occurs during shutdown
+
+	slog.Info("Server shutdown successfully")
 }
